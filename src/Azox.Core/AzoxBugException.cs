a22aca1469981b27ddf1fccd8b70db0f743e174a
Osman YILDIRIM <osmanyildirim@enpos.com.tr>
2022-06-20 13:59:04 +0300

    add base exception

---
+++ src/Azox.Core/AzoxBugException.cs
@@ -0,0 +1,23 @@
+﻿namespace Azox.Core
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class AzoxBugException :
+        AzoxException
+    {
+        #region Ctor
+
+        public AzoxBugException() :
+            base("[BUG]")
+        {
+        }
+
+        public AzoxBugException(string message) :
+            base($"[BUG] {message}")
+        {
+        }
+
+        #endregion Ctor
+    }
+}
